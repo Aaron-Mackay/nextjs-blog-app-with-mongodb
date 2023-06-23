ea9abceb863240b67469fde843ebdc6d28aafea4
Aaron-Mackay <aaron.mackay@coop.co.uk>
2023-06-23 19:13:19 +0100

    CRUD functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,36 +5,44 @@ import PostCard from '../components/PostCard';
 import styles from '../styles/Home.module.css';
 
 export default function Home({ posts }) {
-  return (
-      <div>
-        <Head>
-          <title>Home</title>
-        </Head>
+    return (
+        <div>
+            <Head>
+                <title>Home</title>
+            </Head>
 
-        <Nav />
+            <Nav />
 
-        <main>
-          <div className={styles.container}>
-            {posts.length === 0 ? (
-                <h2>No added posts</h2>
-            ) : (
-                <ul>
-                  {posts.map((post, i) => (
-                      <PostCard post={post} key={i} />
-                  ))}
-                </ul>
-            )}
-          </div>
-        </main>
-      </div>
-  );
+            <main>
+                <div className={styles.container}>
+                    {posts.length === 0 ? (
+                        <h2>No added posts</h2>
+                    ) : (
+                        <ul>
+                            {posts.map((post, i) => (
+                                <PostCard post={post} key={i} />
+                            ))}
+                        </ul>
+                    )}
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export async function getServerSideProps(ctx) {
+    // get the current environment
+    let dev = process.env.NODE_ENV !== 'production';
+    let { DEV_URL, PROD_URL } = process.env;
 
-  return {
-    props: {
-      posts: ['message'],
-    },
-  };
+    // request posts from api
+    let response = await fetch(`${dev ? DEV_URL : PROD_URL}/api/posts`);
+    // extract the data
+    let data = await response.json();
+
+    return {
+        props: {
+            posts: data['message'],
+        },
+    };
 }
