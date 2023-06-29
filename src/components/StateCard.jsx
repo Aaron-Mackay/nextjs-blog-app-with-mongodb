e92b2a46dc45c13dd62b18a6a01fab147de91f2b
Aaron-Mackay <aaron.mackay@coop.co.uk>
2023-06-29 15:25:30 +0100

    Publishing vote

--- src/components/StateCard.jsx
+++ src/components/StateCard.jsx
@@ -1,70 +1,57 @@
-import { useState } from 'react';
-import { useRouter } from 'next/router';
+import {useState} from 'react';
+import {useRouter} from 'next/router';
 
-export default function StateCard({ state }) {
-    const [publishing, setPublishing] = useState(false);
-    const [deleting, setDeleting] = useState(false);
+export default function StateCard({state, user}) {
+    const [selection, setSelection] = useState(state.selection || null)
+    const [voting, setVoting] = useState(false);
     const router = useRouter();
 
-    // Publish state
-    const publishState = async (stateId) => {
-        // change publishing state
-        setPublishing(true);
+    const publishVote = async (state, selection) => {
+        // change voting state
+        setVoting(true);
 
         try {
             // Update state
-            await fetch('/api/states', {
-                method: 'PUT',
-                body: stateId,
+            const res = await fetch('/api/vote', {
+                method: 'PATCH',
+                body: JSON.stringify({
+                    state,
+                    vote: selection,
+                    category: "pres",
+                    userEmail: user.email,
+                    lastVoted: new Date()
+                }),
             });
-
-            // reset the publishing state
-            setPublishing(false);
+            console.log(JSON.stringify(res))
+            // reset the voting state
+            setVoting(false);
 
             // reload the page
             return router.push(router.asPath);
         } catch (error) {
-            // Stop publishing state
-            return setPublishing(false);
+            // Stop voting state
+            return setVoting(false);
         }
     };
-    // Delete state
-    const deleteState = async (stateId) => {
-        //change deleting state
-        setDeleting(true);
 
-        try {
-            console.log(stateId)
-            // Delete state
-            await fetch('/api/states?' + new URLSearchParams({
-                stateId
-            }), {
-                method: 'DELETE'
-            });
-
-            // reset the deleting state
-            setDeleting(false);
-            console.log("deleting")
-
-            // reload the page
-            return router.push(router.asPath);
-        } catch (error) {
-            // stop deleting state
-            return setDeleting(false);
-        }
-    };
+    const onSelect = (e) => {
+        setSelection(e.target.value)
+    }
 
     return (
         <>
-                <h3 style={{textTransform: 'capitalize'}}>{state.state.replace("-", " ")}</h3>
-                {!state.published ? (
-                    <button type="button" onClick={() => publishState(state._id)}>
-                        {publishing ? 'Publishing' : 'Publish'}
-                    </button>
-                ) : null}
-                <button type="button" onClick={() => deleteState(state['_id'])}>
-                    {deleting ? 'Deleting' : 'Delete'}
+            <form onSubmit={() => publishVote(state.state, selection)}>
+                <h3 style={{textTransform: 'capitalize'}}>{state.state.replace("-", " ")} - {selection}</h3>
+                <input type="radio" name={"vote-" + state.state} value="rep" id="rep" checked={selection === "rep"}
+                       onChange={onSelect}/>
+                <label htmlFor="rep">Republican</label>
+                <input type="radio" name={"vote-" + state.state} value="dem" id="dem" checked={selection === "dem"}
+                       onChange={onSelect}/>
+                <label htmlFor="dem">Democrat</label>
+                <button type="submit">
+                    {voting ? 'Voting' : 'Vote'}
                 </button>
+            </form>
         </>
     );
 }
