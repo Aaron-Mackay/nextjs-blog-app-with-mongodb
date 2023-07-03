c9939b48f5b315b040b3e225a1a35d1585d462b8
Aaron-Mackay <aaron.mackay@coop.co.uk>
2023-07-03 22:31:50 +0100

    Added Bootstrap Navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,37 @@
 import styles from '../styles/Nav.module.css';
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Nav({user}) {
     return (
-        <nav className={styles.nav}>
-            <div>
-                {user ? logoutEl(user) : loginEl()}
+        <nav className="navbar navbar-expand-lg fixed-top navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                    <Image src="/election.png" height={34} width={40} alt={"US Election logo"}/>
+                </a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className={styles.item}>
+                            <Link href="/">
+                                Your votes
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/allvotes">
+                                All Votes
+                            </Link>
+                        </li>
+                    </ul>
+
+                </div>
+                <form className="d-flex">
+                    {user ? logoutEl(user) : loginEl()}
+                </form>
             </div>
         </nav>
     );
@@ -13,7 +40,7 @@ export default function Nav({user}) {
 const logoutEl = (user) => {
     return (
         <>
-            Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+            Welcome {user.name}!&nbsp;<a href="/api/auth/logout">Logout</a>
         </>
     )
 }
